74807e378f23d49884465d06cbf95f4e33cbf558
yndvp <58580623+yndvp@users.noreply.github.com>
2021-08-04 15:49:07 +0900

    fix sticky navigation

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
 
 // Modal window
 const openModal = function (e) {
@@ -91,8 +92,18 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 ///////////////////////////////////////
 // Sticky navigation
-window.addEventListener('scroll', function () {
-  const section1TopCoords = section1.getBoundingClientRect().top;
-  if (section1TopCoords <= 0) nav.classList.add('sticky');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
