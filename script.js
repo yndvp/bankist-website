cf02fd354275b4c945c0f54ddaa92988dfac2449
yndvp <58580623+yndvp@users.noreply.github.com>
2021-08-05 10:49:29 +0900

    add functionality of revealing on scroll

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ const tabsContent = document.querySelectorAll('.operations__content');
 const nav = document.querySelector('.nav');
 const header = document.querySelector('.header');
 
+///////////////////////////////////////
 // Modal window
 const openModal = function (e) {
   e.preventDefault();
@@ -35,7 +36,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Scrolling
+// Button scrolling
 btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behavior: 'smooth' });
 });
@@ -93,7 +94,6 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 ///////////////////////////////////////
 // Sticky navigation
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight);
 
 const stickyNav = function (entries) {
   const [entry] = entries;
@@ -107,3 +107,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// Revealing sections on scroll
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionsObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionsObserver.observe(section);
+  section.classList.add('section--hidden');
+});
